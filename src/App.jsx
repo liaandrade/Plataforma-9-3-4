a0cec4e2795888da7e35cdd4a4268146f0f48b62
Lia Andrade <liaandrade895@gmail.com>
2025-02-03 08:39:07 -0300

    Conserta erros do layout inicial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Home from "./home/Home";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
